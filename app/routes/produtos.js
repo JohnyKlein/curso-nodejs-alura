35b3316dc0850b4045c57796b0bbe83cef4bd434
Johny Klein <johnyklein@hotmail.com>
2018-10-02 19:01:11 -0300

    Adicionado conexao com mysql

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,18 @@
 module.exports = function(app) {
-    app.get('/produtos', function(require, response){
-        response.render("produtos/lista");
+    app.get('/produtos', function(req, response){
+        var mysql = require('mysql');
+        var connecetion = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'ibt1705',
+            database: 'livraria_nodejs'
+        });
+
+        connecetion.query('select * from livros', function(erro, resultados){
+            response.send(resultados);
+        });
+
+        connecetion.end();
+
     });
 }
