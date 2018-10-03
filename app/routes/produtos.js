fa0fd8907e45a4bef477bfcfa3c635163f546045
Johny Klein <johnyklein@hotmail.com>
2018-10-02 22:24:04 -0300

    Nova instancia de objeto

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     app.get('/produtos', function(req, response){
         var connection = app.infra.connectionFactory();
-        var produtosBD = app.infra.produtosBD(connection);
+        var produtosBD = new app.infra.produtosBD(connection);
 
         produtosBD.lista(function(erros, resultados){
             response.render("produtos/lista", {lista:resultados})
