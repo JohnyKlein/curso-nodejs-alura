6fd831944ee1c1a4c4d6067b9e3c6309bb3b6ce8
Johny Klein <johnyklein@hotmail.com>
2018-10-02 22:15:47 -0300

    Generalizando classe bd

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,11 +1,22 @@
 module.exports = function(app) {
     app.get('/produtos', function(req, response){
         var connection = app.infra.connectionFactory();
-        var produtosBD = app.infra.produtosBD;
+        var produtosBD = app.infra.produtosBD(connection);
 
-        produtosBD.lista(connection, function(erros, resultados){
+        produtosBD.lista(function(erros, resultados){
             response.render("produtos/lista", {lista:resultados})
         });
+
+        connection.end();
+    });
+    
+    app.get('/produtos/remove', function(req, response){
+        var connection = app.infra.connectionFactory();
+        var produtosBD = app.infra.produtosBD(connection);
+        var produto = produtosBD.carrega(id,callback)
+        if (produto != null) {
+            produtosBD.remove(produto,callback);
+        }
 
         connection.end();
     });
