6d82bdb7706e5e5055f1f4f924db2f4838c00272
Johny Klein <johnyklein@hotmail.com>
2018-10-03 20:48:13 -0300

    Redirect after POST

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,16 @@
 module.exports = function(app) {
-    app.get('/produtos', function(req, response){
-        var connection = app.infra.connectionFactory();
-        var produtosDAO = new app.infra.ProdutosDAO(connection);
+    var listaProdutos = function(req, response){
+            var connection = app.infra.connectionFactory();
+            var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosDAO.lista(function(erros, resultados){
-            response.render("produtos/lista", {lista:resultados})
-        });
+            produtosDAO.lista(function(erros, resultados){
+                response.render("produtos/lista", {lista:resultados})
+            });
 
         connection.end();
-    });
+    };
+
+    app.get('/produtos', listaProdutos);
     
     app.get('/produtos/adicionar', function(req, response){
         response.render("produtos/adiciona");
@@ -20,7 +22,7 @@ module.exports = function(app) {
         
         var produtosDAO = new app.infra.ProdutosDAO(connection);
         produtosDAO.salva(produto, function(erros,resultados){
-            response.render('produtos/lista');
+            response.redirect('/produtos');
         }); 
     });
 }
